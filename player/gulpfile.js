ada291d5abb1cc049d174a8d98a62720f76868ce
Elisée Maurer <elisee@sparklinlabs.com>
2015-12-29 17:58:11 +0100

    Add missing player gulpfile, clean up .gitignore some more

---
+++ player/gulpfile.js
@@ -0,0 +1,26 @@
+var gulp = require("gulp");
+
+// TypeScript
+var ts = require("gulp-typescript");
+var tsProject = ts.createProject("src/tsconfig.json");
+var tslint = require("gulp-tslint");
+
+gulp.task("typescript", function() {
+  var tsResult = tsProject.src()
+    .pipe(tslint({ tslint: require("tslint") }))
+    .pipe(tslint.report("prose", { emitError: false }))
+    .pipe(ts(tsProject));
+  return tsResult.js.pipe(gulp.dest("src/"));
+});
+
+// Browserify
+var browserify = require("browserify");
+var source = require("vinyl-source-stream");
+gulp.task("browserify", [ "typescript" ],function() {
+  var bundler = browserify("./src/index.js");
+  function bundle() { return bundler.bundle().pipe(source("index.js")).pipe(gulp.dest("../public")); }
+  return bundle();
+});
+
+// All
+gulp.task("default", [ "typescript", "browserify" ]);
--- player/src/index.ts
+++ player/src/index.ts
@@ -1,4 +1,4 @@
-/// <reference path="../../../typings/tsd.d.ts" />
+/// <reference path="../../../../typings/tsd.d.ts" />
 
 import * as querystring from "querystring";
 
